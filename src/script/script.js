29271dbceb3137e0849038aac3b6e3f7d2bbcbef
davidemarianoexpleo <126460795+davidemarianoexpleo@users.noreply.github.com>
2024-05-02 12:44:00 +0200

    Fix file js

--- src/script/script.js
+++ src/script/script.js
@@ -9,7 +9,7 @@ const reservationConfirmSection = document.getElementById(
   "reservationConfirmSection"
 );
 
-const endpoint = "";
+const endpoint = "http://localhost:8000/";
 
 reservationCheck.addEventListener("click", () => {
   checkReservation();
@@ -18,13 +18,23 @@ reservationCheck.addEventListener("click", () => {
 const checkReservation = async () => {
   reservationCheck.innerText = "Verifica in corso...";
   reservationCheck.setAttribute("disabled", true);
-  const path = "";
-  const response = await fetch(endpoint + path);
+  const path = "api/check_tavolo";
+  const body = {
+    data: reservationData.value,
+    ora: reservationTime.value,
+  };
+  const response = await fetch(endpoint + path, {
+    body: JSON.parse(body),
+    method: "GET",
+    "Content-type": "application/json",
+  });
   if (response.status == "200") {
     reservationCheck.innerText = "Verifica";
     reservationConfirmSection.classList.remove("hidden");
     reservationCheckSection.classList.add("hidden");
+    const responseJSON = await response.json();
+    console.log(responseJSON);
+  } else {
+    alert("Siamo pieni in questo orario di questo giorno");
   }
-  const responseJSON = await response.json();
-  console.log(responseJSON);
 };
